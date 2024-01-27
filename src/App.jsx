67b559048b597d7945f09d1221ee0cbee8d452a8
Krithik M <krithikm923@gmail.com>
2024-01-27 17:15:22 +0530

    built the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const capCheck = (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    if (name === "thala") {
+      alert("You are the thala cap checker");
+    }
+    else {
+      alert("You are not the thala cap checker");
+    }
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen bg-slate-900 justify-center items-center flex flex-col gap-20">
+        <div >
+          <h1 className="text-4xl font-bold text-white">Are you thala <b className=" text-yellow-300">cap</b> checker</h1>
+        </div>
+        <div>
+          <form onSubmit={capCheck} className="flex flex-col gap-10 bg-slate-800 p-10 rounded ">
+            <label className="text-white text-xl" htmlFor="name">Enter your name : </label>
+            <input type="text" className="bg-gray-900 text-white w-96 h-12 rounded-lg text-center" placeholder="Enter your name" name="name" id="name" />
+            <input type="submit" className="bg-yellow-300 w-96 h-12 rounded-lg text-center" value="Check your cap status " name="submit" id="submit" />
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
